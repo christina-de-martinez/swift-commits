5ad84f427929b30a54c5ebb6e8f5470a3ff12296
Chris Pennington <chris@codinginpublic.dev>
2024-05-24 17:08:10 -0600

    update data file

--- src/data.ts
+++ src/data.ts
@@ -1,62 +1,130 @@
 const data = {
-    annoyed: [
-        "you always knew how to push my buttons.",
-        "if the story's over why am I still writing pages?",
-        "i mean this is exhausting, you know?",
-        "i threw my phone across the room.",
-        "i complained the whole way there.",
+  1989: {
+    fountain: [
+      "You always knew how to push my buttons.",
+      "Cause I knew you were trouble when you walked in, so shame on me now",
+      "And by morning gone was any trace of you, I think I am finally clean.",
+      "And when we go crashing down, we come back every time.",
     ],
-    defeated: [
-        "i should've known.",
-        "horrified looks from everyone in the room.",
-        "i think there's been a glitch, a brief interruption, a slight malfunction.",
-        "i knew you were trouble when you walked in, so shame on me now.",
-        "sometimes giving up is the strong thing.",
-        "the more that you say the less I know.",
-        "got nothing in my brain, that's what people say.",
-        "it's 2 a.m. and I'm cursing your name.",
+    "glitter-gel": [
+      "Got nothing in my brain, that's what people say",
+      "I could show you incredible things, magic, madness, heaven, sin.",
     ],
-    oops: [
-        "you say, I don't understand, and I say, I know you don't.",
-        "it's me, hi, I'm the problem it's me.",
-        "is it too soon to do this yet? Cause I know that it's delicate.",
-        "i can fix him, no really, I can.",
-        "all I feel in my stomach is butterflies.",
-        "long story short, it was a bad time.",
+    quill: [],
+  },
+  evermore: {
+    fountain: [
+      "I complained the whole way there.",
+      "Sometimes, givin' up is the strong thing",
+      "Long story short, it was a bad time",
     ],
-    proud: [
-        "karma is the breeze in my hair on the weekend.",
-        "you are the best thing that's ever been mine.",
-        "long live all the magic we made.",
-        "let's hear the applause.",
-        "when I walk in the room I can still make the whole place shimmer.",
-        "it was all by design, cause I'm a mastermind.",
-        "it's like snow at the beach, weird but it was beautiful.",
-        "who's afraid of little old me? Well, you should be.",
-        "i don't know how it gets better than this.",
+    "glitter-gel": [],
+    quill: [
+      "The more that you say the less I know",
+      "Tell me I've got it wrong somehow.",
     ],
-    relaxed: [
-        "and by morning gone was any trace of you, I think I am finally clean.",
-        "there's a dazzling haze, a mysterious way about you dear.",
-        "the devil's in the details, but you got a friend in me.",
-        "you're on your own, kid, you always have been.",
-        "all there's left to do is run.",
-        "it's gonna be alright, I did my time.",
+  },
+  Fearless: {
+    fountain: [
+      "It's 2 a.m. and I'm cursing your name",
+      "It's the fight of our lives, but we'll stand up champions tonight.",
+      "Wish you could go back and tell yourself what you know now.",
     ],
-    sad: [
-        "i might be okay but I'm not fine at all.",
-        "am I allowed to cry?",
-        "maybe it's me and my blind optimism to blame.",
-        "i might just die, it would make no difference.",
-        "wish you could go back and tell yourself what you know now.",
+    "glitter-gel": ["I don't know how it gets better than this."],
+    quill: ["All there's left to do is run."],
+  },
+  folklore: {
+    fountain: [
+      "Were there clues I didn't see?",
+      'Whispers of "Are you sure?"',
+      "this is me trying",
+      "doing your dirtiest work for you",
     ],
-    yolo: [
-        "they say I did something bad, but why's it feel so good?",
-        "i cry a lot but I am so productive, it's an art.",
-        "i could show you incredible things, magic, madness, heaven, sin.",
-        "no rules in breakable heaven.",
-        "when we go crashing down, we come back every time.",
+    "glitter-gel": [],
+    quill: ["The devil's in the details, but you got a friend in me."],
+  },
+  Lover: {
+    fountain: [
+      "If the story's over why am I still writing pages?",
+      "There's a dazzling haze, a mysterious way about you dear.",
+      "No rules in breakable heaven.",
     ],
+    "glitter-gel": [
+      "I forgot that you existed. It isn't love, it isn't hate, it's just indifference.",
+    ],
+    quill: [],
+  },
+  Midnights: {
+    fountain: [
+      "I think there's been a glitch, a brief interruption, a slight malfunction.",
+      "You're on your own, kid, you always have been.",
+      `You say, "I don't understand," and I say, "I know you don't"`,
+      "Dear reader, burn all the files",
+    ],
+    "glitter-gel": [
+      "Karma is the breeze in my hair on the weekend",
+      "When I walk in the room I can still make the whole place shimmer.",
+    ],
+    quill: [
+      "It's me, hi, I'm the problem it's me",
+      "It was all by design, cause I'm a mastermind.",
+      "It's like snow at the beach, weird but it was beautiful.",
+    ],
+  },
+  Red: {
+    fountain: [
+      "And all I feel In my stomach is butterflies",
+      "I might be okay but I'm not fine at all.",
+    ],
+    "glitter-gel": [
+      "I mean this is exhausting, you know?",
+      "I threw my phone across the room at you.",
+    ],
+    quill: ["As easy as knowing all the words to your old favorite song."],
+  },
+  Reputation: {
+    fountain: [
+      "Is it too soon to do this yet? Cause I know that it's delicate",
+      "Don't blame me, love made me crazy",
+      "They say I did something bad, but why's it feel so good?",
+      "all the pieces fall right into place",
+      "I got smarter, I got harder in the nick of time",
+      "I've made mistakes and made some choices, that's hard to deny",
+    ],
+    "glitter-gel": [],
+    quill: [],
+  },
+  "Speak Now": {
+    fountain: [
+      "I should've known.",
+      "You are the best thing that's ever been mine",
+      "Long live all the magic we made",
+      "Let's hear the applause",
+    ],
+    "glitter-gel": ["Horrified looks from everyone in the room."],
+    quill: [],
+  },
+  "The Tortured Poets Department": {
+    fountain: [
+      "I can fix him, no really, I can",
+      "Who's afraid of little old me? Well, you should be.",
+      "it's gonna be alright, I did my time.",
+      "I might just die, it would make no difference.",
+    ],
+    "glitter-gel": [
+      "Am I allowed to cry?",
+      "I cry a lot but I am so productive, it's an art.",
+    ],
+    quill: [],
+  },
+  "Taylor Swift": {
+    fountain: [
+      "I'm still trying to figure it out",
+      "the time taken up, but there's never enough",
+    ],
+    "glitter-gel": [],
+    quill: [],
+  },
 } as const;
 
 export default data;
